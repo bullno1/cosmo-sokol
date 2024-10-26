0caf4a7cbbe4ced2521abb7cca358883bc250cc2
Bach Le <bach@bullno1.com>
2024-10-26 08:14:33 +0800

    Make IMGUI work

--- main.c
+++ main.c
@@ -6,14 +6,16 @@
 //------------------------------------------------------------------------------
 #define SOKOL_GLCORE
 #define SOKOL_IMPL
+#define dlopen cosmo_dlopen
+#define dlsym cosmo_dlsym
 #include "sokol_app.h"
 #include "sokol_gfx.h"
 #include "sokol_log.h"
 #include "sokol_glue.h"
-/*#define CIMGUI_DEFINE_ENUMS_AND_STRUCTS*/
-/*#include "cimgui.h"*/
-/*#define SOKOL_IMGUI_IMPL*/
-/*#include "util/sokol_imgui.h"*/
+#define CIMGUI_DEFINE_ENUMS_AND_STRUCTS
+#include "cimgui.h"
+#define SOKOL_IMGUI_IMPL
+#include "util/sokol_imgui.h"
 
 typedef struct {
     uint64_t last_time;
@@ -32,9 +34,9 @@ void init(void) {
 
     // use sokol-imgui with all default-options (we're not doing
     // multi-sampled rendering or using non-default pixel formats)
-    /*simgui_setup(&(simgui_desc_t){*/
-        /*.logger.func = slog_func,*/
-    /*});*/
+    simgui_setup(&(simgui_desc_t){
+        .logger.func = slog_func,
+    });
 
     /* initialize application state */
     state = (state_t) {
@@ -49,53 +51,53 @@ void init(void) {
 }
 
 void frame(void) {
-    /*const int width = sapp_width();*/
-    /*const int height = sapp_height();*/
-    /*simgui_new_frame(&(simgui_frame_desc_t){*/
-        /*.width = width,*/
-        /*.height = height,*/
-        /*.delta_time = sapp_frame_duration(),*/
-        /*.dpi_scale = sapp_dpi_scale()*/
-    /*});*/
+    const int width = sapp_width();
+    const int height = sapp_height();
+    simgui_new_frame(&(simgui_frame_desc_t){
+        .width = width,
+        .height = height,
+        .delta_time = sapp_frame_duration(),
+        .dpi_scale = sapp_dpi_scale()
+    });
 
     /*// 1. Show a simple window*/
     /*// Tip: if we don't call ImGui::Begin()/ImGui::End() the widgets appears in a window automatically called "Debug"*/
-    /*static float f = 0.0f;*/
-    /*igText("Hello, world!");*/
-    /*igSliderFloat("float", &f, 0.0f, 1.0f, "%.3f", ImGuiSliderFlags_None);*/
-    /*igColorEdit3("clear color", (float*)&state.pass_action.colors[0].clear_value, 0);*/
-    /*if (igButton("Test Window", (ImVec2) { 0.0f, 0.0f})) state.show_test_window ^= 1;*/
-    /*if (igButton("Another Window", (ImVec2) { 0.0f, 0.0f })) state.show_another_window ^= 1;*/
-    /*igText("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / igGetIO()->Framerate, igGetIO()->Framerate);*/
+    static float f = 0.0f;
+    igText("Hello, world!");
+    igSliderFloat("float", &f, 0.0f, 1.0f, "%.3f", ImGuiSliderFlags_None);
+    igColorEdit3("clear color", (float*)&state.pass_action.colors[0].clear_value, 0);
+    if (igButton("Test Window", (ImVec2) { 0.0f, 0.0f})) state.show_test_window ^= 1;
+    if (igButton("Another Window", (ImVec2) { 0.0f, 0.0f })) state.show_another_window ^= 1;
+    igText("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / igGetIO()->Framerate, igGetIO()->Framerate);
 
     /*// 2. Show another simple window, this time using an explicit Begin/End pair*/
-    /*if (state.show_another_window) {*/
-        /*igSetNextWindowSize((ImVec2){200,100}, ImGuiCond_FirstUseEver);*/
-        /*igBegin("Another Window", &state.show_another_window, 0);*/
-        /*igText("Hello");*/
-        /*igEnd();*/
-    /*}*/
+    if (state.show_another_window) {
+        igSetNextWindowSize((ImVec2){200,100}, ImGuiCond_FirstUseEver);
+        igBegin("Another Window", &state.show_another_window, 0);
+        igText("Hello");
+        igEnd();
+    }
 
     /*// 3. Show the ImGui test window. Most of the sample code is in ImGui::ShowDemoWindow()*/
-    /*if (state.show_test_window) {*/
-        /*igSetNextWindowPos((ImVec2){460,20}, ImGuiCond_FirstUseEver, (ImVec2){0,0});*/
-        /*igShowDemoWindow(0);*/
-    /*}*/
+    if (state.show_test_window) {
+        igSetNextWindowPos((ImVec2){460,20}, ImGuiCond_FirstUseEver, (ImVec2){0,0});
+        igShowDemoWindow(0);
+    }
 
     // the sokol_gfx draw pass
     sg_begin_pass(&(sg_pass){ .action = state.pass_action, .swapchain = sglue_swapchain() });
-    /*simgui_render();*/
+    simgui_render();
     sg_end_pass();
     sg_commit();
 }
 
 void cleanup(void) {
-    /*simgui_shutdown();*/
+    simgui_shutdown();
     sg_shutdown();
 }
 
 void input(const sapp_event* event) {
-    /*simgui_handle_event(event);*/
+    simgui_handle_event(event);
 }
 
 sapp_desc sokol_main(int argc, char* argv[]) {
