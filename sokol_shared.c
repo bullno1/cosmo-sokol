d97c494212991a055696a616eb017faa63d19764
Bach Le <bach@bullno1.com>
2024-10-26 19:41:41 +0800

    Generate platform specific code for sokol on Linux

---
+++ sokol_shared.c
@@ -0,0 +1,8 @@
+#define SOKOL_GLCORE
+#include "sokol_app.h"
+#define SOKOL_IMPL
+#define dlopen cosmo_dlopen
+#define dlsym cosmo_dlsym
+#include "sokol_gfx.h"
+#include "sokol_log.h"
+#include "sokol_glue.h"
